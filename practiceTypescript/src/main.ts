7823a75ff4101b6d8b0e0c73a2c6c83b123bc600
Jaehyun Kim <68116156+kdjames0930@users.noreply.github.com>
2025-03-21 11:47:52 +0000

    Prevent redundant list

--- practiceTypescript/src/main.ts
+++ practiceTypescript/src/main.ts
@@ -11,17 +11,26 @@ const initApp = (): void => {
     itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
         event.preventDefault()
 
+        let newVal = true
         const input = document.getElementById("newItem") as HTMLInputElement
         const newEntryText: string = input.value.trim()
+        input.value = ''
         if(!newEntryText.length) return
 
         const itemId: number = fullList.list.length
             ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
             : 1
 
+        fullList.list.forEach(element => {
+            if(element.item === newEntryText) {
+                alert("You have already added this goal!")
+                newVal = false
+            }
+        });
+
         const newItem = new ListItem(itemId.toString(), newEntryText)
 
-        fullList.addItem(newItem)
+        if(newVal) fullList.addItem(newItem)
 
         template.render(fullList)
     })
